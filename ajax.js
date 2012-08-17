fe2a3a28e20e2817f36142ad59c238553d1312f6
aysee <630999015@qq.com>
2012-08-17 15:40:25 +0800

    ajax-package

---
+++ ajax.js
@@ -0,0 +1,17 @@
+function ajax(url,fnResult,fnFailure){
+	var xmlhttp;
+	if(window.XMLHttpRequest){
+		xmlhttp=new XMLHttpRequest();
+	}else{
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
+	xmlhttp.onreadystatechange=function(){
+		if(xmlhttp.readyState==4 && xmlhttp.status==200){
+			fnResult(xmlhttp.responseText);
+		}else if(xmlhttp.status!=200 && fnFailure){
+			fnFailure("数据读取失败");
+		}
+	}
+}
